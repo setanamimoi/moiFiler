e59029e953325e39cd042c1ed84a317180a0623d
setanamimoi <setanamimoi@gmail.com>
2013-04-23 20:02:07 +0900

    初期ファイルを配置

---
+++ _Tests/Program.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Gui;
+using System;
+using System.Windows.Forms;
+
+namespace _Tests
+{
+    public static class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            AppEntry.Main(new string[] { Application.ExecutablePath });
+        }
+    }
+}
